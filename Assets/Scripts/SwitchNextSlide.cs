5fd27ea87350e8c6f8ee93afdb9d6c22db87af1e
Maxim Yacenko <m.yacenko@gmail.com>
2018-05-07 21:13:43 +0300

    123

--- Assets/Scripts/SwitchNextSlide.cs
+++ Assets/Scripts/SwitchNextSlide.cs
@@ -2,27 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Text.RegularExpressions;
 public class SwitchNextSlide : MonoBehaviour {
-
-    public Sprite[] images;
-    public string[] texts;
-    public Image picture;
+    //URL: https://pdd.am.ru/road-signs/preduprezhdajushhie-znaki/
+    public TextAsset spellData;
     public Text subj;
+    string[] lines;
+    public RawImage rImg;
+    string imagePath = "textures";
+    Texture2D[] typedTextures;
     void Start()
     {
+        Object[] textures = Resources.LoadAll(imagePath);
+        typedTextures = new Texture2D[textures.Length];
+        for (int i = 0; i < textures.Length; i++)
+        {
+            typedTextures[i] = (Texture2D)textures[i];
+        }
+        string fs = spellData.text;
+        lines = Regex.Split(fs, "-----");
         StartCoroutine(switcher());
+
     }
+
 
     public IEnumerator switcher()
     {
-        for(int i = 0; i < images.Length; i++)
+        subj.text = "Начало занятия";
+        for (int i = 0; i < lines.Length; i++)
         {
-            picture.sprite = images[i];
-            subj.text = texts[i];
-            yield return new WaitForSeconds(15.0f);
+            subj.text = lines[i];
+            if (typedTextures.Length > i)
+            {
+                rImg.texture = typedTextures[i];
+            }
+            
+            yield return new WaitForSeconds(7.0f);
         }
-        picture.enabled = false;
+        rImg.enabled = false;
         subj.text = "Конец занятия";
+        
     }
 }
     
